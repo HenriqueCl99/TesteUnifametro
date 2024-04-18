e393747cf2f45d1d519c1fd14ccbd68dc2247a63
Henrique Calixto <prghenriquetec@gmail.com>
2024-04-18 06:22:00 -0300

    Concluido todo o teste de leitura e validação dos dados, tratado o arquivo, e criado um relatório mostrando as informações solicitadas.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Teste</title>
+</head>
+<body> 
+    Rodando relatorios...
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
